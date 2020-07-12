a59bc8d103e228f1ac0d49ad9ec27ee74b299c90
TeddyPgadie <48135408+TeddyPgadie@users.noreply.github.com>
2020-07-11 22:36:14 -0400

    Updated Js
    - Apply focus on the name text field as soon the page load
    - Hide the input field 'other' from the drop down menu.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,3 @@
-document.getElementById("name").focus(); // Puts focus on the first text field when the page load.
+document.getElementById("name").focus();                 // Puts focus on the first text field when the page load.
+document.getElementById('title').children[5].hidden = true; //Hide Other from the drop down menu
 
